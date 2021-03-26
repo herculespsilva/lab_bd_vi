ce9a302e468b5e5541784ca21077b996d2bbe9a4
Hércules Pereira <herculespsilva@hotmail.com>
2021-03-26 20:38:00 -0300

    Fase Verde

--- Pratica_01/app/src/main/java/model/Calculadora.java
+++ Pratica_01/app/src/main/java/model/Calculadora.java
@@ -2,11 +2,11 @@ package model;
 
 public class Calculadora implements Funcao {
 	
-	 /* Método que obrigatoriamente devera ser implementado pela
+	 /* MÃ©todo que obrigatoriamente devera ser implementado pela
     classe Calculadora que sera implementada pela Interface */
 	
 	@Override
 	public float div (float x, float y) {
-		 return x;
+		 return (x / y);
 	 } 
 }
