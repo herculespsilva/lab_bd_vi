56f1807f187e01602143cd47e66bb68d73955546
herculespsilva <herculespsilva@hotmail.com>
2021-05-21 23:40:43 +0000

    Fase Verde

--- Pratica_02/app/src/main/java/model/Calculadora.java
+++ Pratica_02/app/src/main/java/model/Calculadora.java
@@ -2,15 +2,23 @@ package model;
 
 public class Calculadora implements Funcao {
 	
-	 /* Método que obrigatoriamente devera ser implementado pela
+	 /* MÃ©todo que obrigatoriamente devera ser implementado pela
     	    classe Calculadora que sera implementada pela Interface */
 	
 	@Override
 	public float div (float x, float y) {
 		if( (x >=-100 && x <=100) && (y >=-100 && y <=100) ){
-			return x;
+			return x / y;
 		}
-	 	
-		return x;
-	} 
+		else {
+			if(x <-100 || x >100) {
+		   		System.out.println("valor" + x + "informado esta fora do limite permitido!");
+				return 0;
+			} 
+			else {
+				System.out.println("valor" + y + "informado esta fora do limite permitido!");
+				return 0;
+			}
+		}
+	 } 
 }
